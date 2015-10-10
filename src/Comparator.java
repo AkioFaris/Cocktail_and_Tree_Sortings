6e65b049aeef4647e387a2b81d3a6a624810deea
AkioFaris <rnanimo@gmail.com>
2015-10-10 19:42:36 +0300

    old version of files were deleted

--- src/Comparator.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Akio on 18/09/2015.
- */
-public interface Comparator<T> {
-  int compare(T obj1, T obj2);
-}
--- src/Sorting.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Akio on 18/09/2015.
- */
-public interface Sorting<T> {
-  void sort(final T[] array, Comparator<T> cmp);
-}
